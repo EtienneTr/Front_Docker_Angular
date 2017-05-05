f9b38bc84a32baa142f41fa997584c00988a19ef
EtienneT <tienou42@gmail.com>
2017-05-05 14:27:29 +0200

    shell

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -2,9 +2,14 @@
 cd /tmp
 
 #Clone or pull if exist (auto update on restart container)
-if cd Front_Docker_Angular; then git pull; else git clone https://github.com/EtienneTr/Front_Docker_Angular; fi
+if [ -d "Front_Docker_Angular" ]; then 
+	cd Front_Docker_Angular
+	git pull; 
+else 
+	git clone https://github.com/EtienneTr/Front_Docker_Angular
+	cd Front_Docker_Angular;
+fi
 
-cd Front_Docker_Angular
 npm install
 npm start
 
